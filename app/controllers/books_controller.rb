2d22fe1f9c4930e269ea9804a17aea862fd7fa79
EC2 Default User <ec2-user@ip-172-31-9-83.ap-northeast-1.compute.internal>
2023-03-28 12:50:58 +0000

    課題訂正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,27 @@
 class BooksController < ApplicationController
-  def top
-  end
   def index
-    @books = Book.all.order("id ASC")
-    @book = Book.new
+    @book = Book.new 
+    @books = Book.all 
   end
+
+  def show
+    @book = Book.find(params[:id]) 
+  end
+
+  def edit
+    @book = Book.find(params[:id]) 
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "successfully" 
+    redirect_to book_path(params[:id])
+    else
+    render :edit
+    end
+  end
+
 
   def create
     @book = Book.new(book_params)
@@ -16,28 +33,15 @@ class BooksController < ApplicationController
     end
   end
 
-  def show
-    @book = Book.find(params[:id])
-  end
-
-  def edit
-    @book = Book.find(params[:id])
-  end
-
-  def update
-    book = Book.find(params[:id])
-    flash[:notice] = "successfully" if book.update(book_params)
-    redirect_to book_path(params[:id])
-  end
-
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to book_path(params[:id])
+    redirect_to books_path
   end
 
   private
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  end
+end
